29f4d834e74d8c9465da69661e0b3dfc29d88a8e
Driftocity <69994669+Driftocity@users.noreply.github.com>
2026-06-23 23:30:21 -0400

    Add Walls component to HouseScene

--- src/components/HouseScene.jsx
+++ src/components/HouseScene.jsx
@@ -14,14 +14,51 @@ function Floor() {
   )
 }
 
+function Walls() {
+  const width = useHouseStore((s) => s.width)
+  const length = useHouseStore((s) => s.length)
+  const height = useHouseStore((s) => s.height)
+
+  const wallThickness = 0.3
+
+  return (
+    <group>
+      {/* Front Wall */}
+      <mesh position={[0, height / 2, length / 2]}>
+        <boxGeometry args={[width, height, wallThickness]} />
+        <meshStandardMaterial color="#d1d5db" />
+      </mesh>
+
+      {/* Back Wall */}
+      <mesh position={[0, height / 2, -length / 2]}>
+        <boxGeometry args={[width, height, wallThickness]} />
+        <meshStandardMaterial color="#d1d5db" />
+      </mesh>
+
+      {/* Left Wall */}
+      <mesh position={[-width / 2, height / 2, 0]}>
+        <boxGeometry args={[wallThickness, height, length]} />
+        <meshStandardMaterial color="#d1d5db" />
+      </mesh>
+
+      {/* Right Wall */}
+      <mesh position={[width / 2, height / 2, 0]}>
+        <boxGeometry args={[wallThickness, height, length]} />
+        <meshStandardMaterial color="#d1d5db" />
+      </mesh>
+    </group>
+  )
+}
+
 export default function HouseScene() {
   return (
-    <div style={{ height: "400px", width: "100%" }}>
-      <Canvas camera={{ position: [10, 10, 10] }}>
+    <div style={{ height: "500px", width: "100%" }}>
+      <Canvas camera={{ position: [12, 12, 12] }}>
         <ambientLight intensity={0.6} />
-        <directionalLight position={[5, 10, 5]} />
+        <directionalLight position={[10, 15, 10]} intensity={1} />
 
         <Floor />
+        <Walls />
 
         <OrbitControls />
       </Canvas>
